d74b9016538397c3ba614c9c50fb913379a10fd3
YovoTWW <yovonedelchev@gmail.com>
2025-02-15 21:02:27 +0200

    Update Product.cs

--- YMart/YMart/Data/Models/Product.cs
+++ YMart/YMart/Data/Models/Product.cs
@@ -2,5 +2,19 @@
 {
     public class Product
     {
+        public Product()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public int Quantity {  get; set; }
     }
 }
