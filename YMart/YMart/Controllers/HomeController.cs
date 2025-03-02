46c49c83dac68c2e7ce72804e48c4bc9ad9e4a17
YovoTWW <yovonedelchev@gmail.com>
2025-03-02 17:46:19 +0200

    Added new Functionalities based on if the user is signed in or not

--- YMart/YMart/Controllers/HomeController.cs
+++ YMart/YMart/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace YMart.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult NotSignedIn()
+        {
+            return this.View();
+        }
     }
 }
